94376afe59d9243ae0dceb47715672e52560f884
Joshua Clayton <jclayton@thoughtbot.com>
2011-03-07 11:39:58 -0500

    Insert/update initial

--- ext/faststep/collection.c
+++ ext/faststep/collection.c
@@ -1,5 +1,6 @@
 #include "collection.h"
 #include "mongo.h"
+#include <string.h>
 
 void collection_main(VALUE faststep) {
   VALUE FaststepCollection = rb_define_class_under(faststep, "Collection", rb_cObject);
@@ -8,6 +9,8 @@ void collection_main(VALUE faststep) {
 
   rb_define_method(FaststepCollection, "initialize", collection_init, 2);
   rb_define_method(FaststepCollection, "count",      collection_count, -1);
+  rb_define_method(FaststepCollection, "insert",     collection_insert, 1);
+  rb_define_method(FaststepCollection, "update",     collection_update, 2);
   return;
 }
 
@@ -19,10 +22,14 @@ VALUE collection_init(VALUE self, VALUE name, VALUE db) {
 }
 
 void init_bson_from_ruby_hash(bson* bson, VALUE hash) {
-  VALUE rb_bson = rb_const_get(rb_cObject, rb_intern("BSON"));
-  VALUE byte_buffer = rb_funcall(rb_bson, rb_intern("serialize"), 3, hash, Qfalse, Qfalse);
-  VALUE query = rb_funcall(byte_buffer, rb_intern("to_s"), 0);
-  bson_init(bson, RSTRING_PTR(query), 1);
+  if(NIL_P(hash)) {
+    bson_empty(bson);
+  } else {
+    VALUE rb_bson = rb_const_get(rb_cObject, rb_intern("BSON"));
+    VALUE byte_buffer = rb_funcall(rb_bson, rb_intern("serialize"), 3, hash, Qfalse, Qfalse);
+    VALUE query = rb_funcall(byte_buffer, rb_intern("to_s"), 0);
+    bson_init(bson, RSTRING_PTR(query), 1);
+  }
   return;
 }
 
@@ -41,5 +48,53 @@ VALUE collection_count(int argc, VALUE* argv, VALUE self) {
     init_bson_from_ruby_hash(bson_query, query);
   }
 
-  return ULL2NUM(mongo_count(conn, "test", RSTRING_PTR(rb_iv_get(self, "@name")), bson_query));
+  VALUE result = ULL2NUM(mongo_count(conn, "test", RSTRING_PTR(rb_iv_get(self, "@name")), bson_query));
+  bson_destroy(bson_query);
+  return result;
+}
+
+void collection_ns(char* ns, char* database, char* collection) {
+  strcat(ns, database);
+  strcat(ns, ".");
+  strcat(ns, collection);
+  return;
+}
+
+VALUE collection_insert(VALUE self, VALUE document) {
+  mongo_connection* conn;
+  Data_Get_Struct(rb_iv_get(self, "@db"), mongo_connection, conn);
+
+  bson* bson_document = malloc(sizeof(bson));
+
+  char ns[255];
+  collection_ns(ns, "test", RSTRING_PTR(rb_iv_get(self, "@name")));
+
+  init_bson_from_ruby_hash(bson_document, document);
+
+  mongo_insert(conn, ns, bson_document);
+
+  bson_destroy(bson_document);
+
+  return Qtrue;
+}
+
+VALUE collection_update(VALUE self, VALUE query, VALUE operations) {
+  mongo_connection* conn;
+  Data_Get_Struct(rb_iv_get(self, "@db"), mongo_connection, conn);
+
+  bson* bson_query      = malloc(sizeof(bson));
+  bson* bson_operations = malloc(sizeof(bson));
+
+  char ns[255];
+  collection_ns(ns, "test", RSTRING_PTR(rb_iv_get(self, "@name")));
+
+  init_bson_from_ruby_hash(bson_query, query);
+  init_bson_from_ruby_hash(bson_operations, operations);
+
+  mongo_update(conn, ns, bson_query, bson_operations, MONGO_UPDATE_MULTI);
+
+  bson_destroy(bson_query);
+  bson_destroy(bson_operations);
+
+  return Qtrue;
 }
--- ext/faststep/collection.h
+++ ext/faststep/collection.h
@@ -6,5 +6,8 @@
 void collection_main(VALUE);
 VALUE collection_init(VALUE, VALUE, VALUE);
 VALUE collection_count(int, VALUE*, VALUE);
+VALUE collection_insert(VALUE, VALUE);
+VALUE collection_update(VALUE, VALUE, VALUE);
+void collection_ns(char*, char*, char*);
 void init_bson_from_ruby_hash(bson*, VALUE);
 #endif
